876d6f48158bd46f21a33d059c16e32b72071992
Jazmine Lavasani <36315433+JazKatz@users.noreply.github.com>
2021-03-02 17:16:59 -0700

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MyFirstProgram {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Jazmine Lavasani 21");
+		System.out.println("Welcome to my First Program");
+		}
+}
